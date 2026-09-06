14a25db620d56fd79452c57c410af1c13792dab4
kinopiix0120 <pikachu.bibibi@gmail.com>
2026-09-06 20:12:16 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+var CACHE_NAME = "kanribangou-v1";  //キャッシュ名。PWAごとに変える。
+
+var FILES = [
+  "./",
+  "index.html",
+  "style.css",
+  "script.js",
+  "qrcode.min.js",
+  "manifest.json",
+  "icon-192.png"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(FILES);
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.map(function (key) {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
